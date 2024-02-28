58266e2d517b77a95f6d6821a0bbb5e89b26861c
username <wecp-dummy@gmail.com>
2024-02-28 11:53:03 +0000

    dashboard done

--- client/src/app/dashbaord/dashbaord.component.ts
+++ client/src/app/dashbaord/dashbaord.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-dashbaord',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashbaord.component.scss']
 })
 export class DashbaordComponent {
+  showUserTable:boolean=true;
+  userList$: Observable<any>;
+  eventList$: Observable<any>;
+  constructor(private httpService:HttpService){
+    
+  }
+  ngOnInit(): void {
 
+    // this.getEvent();
+    this.getUsers();
+    this.getEvents();
+  }
+  getEvents() {
+    this.eventList$=this.httpService.getEvents();
+  }
+  getUsers() {
+     //complete this function
+     this.userList$  = this.httpService.getAllUser();
+  }
 }
