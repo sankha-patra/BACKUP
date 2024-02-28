45e097f972a1e1338b311f0cf1598a79872af57d
username <wecp-dummy@gmail.com>
2024-02-28 06:13:18 +0000

    login added

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpService } from '../../services/http.service';
 import { AuthService } from '../../services/auth.service';
@@ -11,27 +11,51 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit{
   itemForm: FormGroup;
   formModel:any={};
   showError:boolean=false;
   errorMessage:any;
-  constructor(public router:Router, public httpService:HttpService, private formBuilder: FormBuilder, private authService:AuthService) 
+  constructor(public router:Router, private formBuilder: FormBuilder)
     {
       this.itemForm = this.formBuilder.group({
-        //compelete this 
-       
+        //compelete this
+        username : ["", [Validators.required, this.noSpaceValidations]],
+        password : ["", [Validators.required, this.passwordIsNotValid]]
     });
   }
-
+ 
+ 
+  passwordIsNotValid(control : AbstractControl) : {[key : string] : boolean} | null
+  {
+    let passwordVal = control.value as string;
+    let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])([a-zA-Z\d!@#%$%^&*]){8,}$/;
+    if(!passwordRegex.test(passwordVal))
+    {
+      return {invalidPassword : true};
+    }
+    return null;
+  }
+ 
+ 
+  noSpaceValidations(control: AbstractControl): ValidationErrors | null {
+    const controlValue = control.value as string;
+ 
+    if (controlValue.indexOf(' ')>= 0) {
+      return {  NoSpaceValidator: true };
+    } else {
+      return null;
+    }
+  }
+ 
   ngOnInit(): void {
   }
   onLogin() {
-  //compelete this 
+  //compelete this
 }
-
+ 
 registration()
   {
-     //compelete this 
+     //compelete this
   }
 }
